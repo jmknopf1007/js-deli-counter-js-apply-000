6e03ea8617d69d0eb5fdfbfb1167f3c331a9c8de
Jacob Knopf <jmknopf1007@gmail.com>
2019-09-09 19:25:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var katzDeliLine = []; /*At the beginning of the day,
 the deli is empty and is represented by an empty array*/
 
+var number = 1 + katzDeliLine.length; 
+
+function takeANumber(katzDeliLine) {
+  katzDeliLine.push(`${number}`);
+  return(`Welcome you are number ${number}.`);
+}
+
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(`${name}`);
   return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 }
+
 function nowServing(katzDeliLine) {
   let i = 0;
   while (i < katzDeliLine.length) {
@@ -18,11 +26,11 @@ function nowServing(katzDeliLine) {
 }
 
 var currentLine = function (katzDeliLine) {
-  if (katzDeliLine.length===0) {
+  if (katzDeliLine.length === 0) {
   return "The line is currently empty.";
 }
    var customerAndNumber = [];
-   for (var i=0; i<katzDeliLine.length; i++) {
+   for (var i = 0; i < katzDeliLine.length; i++) {
    customerAndNumber.push(i+1 + ". " + katzDeliLine[i])
 }
   return "The line is currently: " + customerAndNumber.join(", ");
